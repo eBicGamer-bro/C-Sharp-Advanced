78a13991b9f97c7f447634e82487b92989d5a7ef
Fares Fouad <faresfouadhussein90@gmail.com>
2026-04-05 20:42:56 +0200

    Add files via upload

---
+++ Assignment 1 C# Advanced/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Assignment_1_C__Advanced
+{
+    public class InsufficientBalanceException : Exception
+    {
+        public InsufficientBalanceException()
+        {
+
+        }
+        public InsufficientBalanceException(string message) : base(message)
+        {
+        }
+        public InsufficientBalanceException(string? message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+    public class BankAccount
+    {
+        private string name;
+        private double balance;
+        public BankAccount(string name, double balance)
+        {
+            this.name = name;
+            this.balance = balance;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > balance)
+                throw new InsufficientBalanceException("Insufficient Balance!");
+            else balance -= amount;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Fares", 900);
+            try
+            {
+                account.Withdraw(1000);
+            }catch(InsufficientBalanceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
