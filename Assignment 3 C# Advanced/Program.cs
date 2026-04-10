f01faa7ae61c5a14cbf1a405d43c5ee79c30a159
Fares Fouad <faresfouadhussein90@gmail.com>
2026-04-10 19:15:15 +0200

    Add files via upload

---
+++ Assignment 3 C# Advanced/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment_3_C__Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = Enumerable.Range(2, 19).Where(n => n % 2 == 0);
+            foreach(var number in num)
+            {
+                Console.WriteLine(number);
+            }
+            var welcome = Enumerable.Repeat("Hello", 5);
+            foreach (var wel in welcome)
+            {
+                Console.WriteLine(wel);
+            }
+        }
+    }
+}
