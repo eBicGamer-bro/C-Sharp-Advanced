3fdf670189f7c119d8cd357cd8d07dfbe95b21d9
Fares Fouad <faresfouadhussein90@gmail.com>
2026-04-10 18:38:22 +0200

    Add files via upload

---
+++ Assignment 2 C# Advanced/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment_2_C__Advanced
+{
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 89, 99 };
+            Func<int, int> half = n => n / 2;   //I dont think it can be done using predicate as predicate returns bool
+            var halfed = nums.Select(half);
+            foreach(var number in halfed)
+            {
+                Console.WriteLine(number);
+            }
+           
+        }
+    }
+}
